3c3483af61722c166e9d7ca59e4e010870d679c4
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-09-28 17:19:53 -0500

    updates

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict:
-        return (max(my_dict))
+def best_score(a_dictionary):
+    if a_dictionary:
+        return(max(a_dictionary, key=a_dictionary.get))
